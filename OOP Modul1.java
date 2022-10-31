83aa8a46ea365bd2b86f950856848058eb04a9c8
NurainiFauziyah <116267675+NurainiFauziyah@users.noreply.github.com>
2022-10-31 08:41:49 +0700

    Add files via upload

---
+++ OOP Modul1.java
@@ -0,0 +1,14 @@
+public class makanan {
+    string Name;
+    string No_Handphone;
+
+    public void setName(string Name){
+        this.name = Name;
+    }
+    public string getNo_Handphone(){
+        this.No_Handphone= No_Handphone;
+        System.out.printIn("Selamat Datang di Restoran EAD");
+        System.out.printIn("Silahkan register terlebih dahulu");
+        System.out.printIn("=================================");
+    }
+}
